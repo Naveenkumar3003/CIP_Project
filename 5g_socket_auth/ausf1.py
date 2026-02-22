1abb7079a0efa9daec4d9397ee509435720b8665
DHARSHINI K <dharshinidharshu575@gmail.com>
2026-02-22 10:11:56 +0530

    Update ausf1.py

--- 5g_socket_auth/ausf1.py
+++ 5g_socket_auth/ausf1.py
@@ -14,25 +14,26 @@ print("AUSF Running...")
 conn, addr = server.accept()
 data = json.loads(conn.recv(4096).decode())
 
-print("\n[STEP 2] AMF → AUSF : Authentication Data Request")
+print(f"\n[STEP 2] AMF -> AUSF : Authentication Data Request (SUCI={data['SUCI']})")
 
 udm = socket.socket()
 udm.connect((HOST, UDM_PORT))
 
-print("[STEP 3] AUSF → UDM : Fetch Subscriber Data")
+print(f"[STEP 3] AUSF -> UDM : Fetch Subscriber Data (SUCI={data['SUCI']})")
 
 udm.send(json.dumps(data).encode())
 auth_vector = json.loads(udm.recv(4096).decode())
 udm.close()
 
-print("[STEP 5] AUSF → AMF : Forwarding Authentication Vector")
+print(f"[STEP 5] AUSF -> AMF : Forwarding RAND ({auth_vector['RAND']}), AUTN ({auth_vector['AUTN']})")
+
 conn.send(json.dumps(auth_vector).encode())
 
 res_data = json.loads(conn.recv(4096).decode())
 
 print("\n[AUSF] Verification Phase")
-print("Received RES* =", res_data["RES"])
-print("Stored XRES*  =", auth_vector["XRES"])
+print(f"Received RES* ({res_data['RES']})")
+print(f"Stored XRES*  ({auth_vector['XRES']})")
 print("Checking RES* == XRES*")
 
 if res_data["RES"] == auth_vector["XRES"]:
